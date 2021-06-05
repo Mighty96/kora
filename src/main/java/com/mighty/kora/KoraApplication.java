616fdbc4336f5679bcf8822161bec412c10b9f28
Mighty96 <mightyspade96@gmail.com>
2021-06-05 14:46:06 +0900

    First commit

---
+++ src/main/java/com/mighty/kora/KoraApplication.java
@@ -0,0 +1,13 @@
+package com.mighty.kora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoraApplication.class, args);
+	}
+
+}
