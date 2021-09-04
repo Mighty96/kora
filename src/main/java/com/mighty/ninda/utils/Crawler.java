2a36b6c6cfde369d69ca254e31c407f75b91bef2
Mighty96 <mightyspade96@gmail.com>
2021-09-04 18:08:55 +0900

    saleCrawl 날짜관련 로직 수정

--- src/main/java/com/mighty/ninda/utils/Crawler.java
+++ src/main/java/com/mighty/ninda/utils/Crawler.java
@@ -110,7 +110,7 @@ public class Crawler {
 
         Document gameDoc = Jsoup.connect(gameUrl).timeout(30000).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36").get();
 
-        Elements prices = gameDoc.getElementsByClass("special-price");
+        Elements prices = gameDoc.getElementsByClass("eshop-price-special_period");
         String price = prices.select(".price").get(0).text();
 
         String saleDate = gameDoc.getElementsByClass("special-period").get(0).text();
